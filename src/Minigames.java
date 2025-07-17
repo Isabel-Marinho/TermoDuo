4e90843edc8ef34037281467ad60518782579672
Isabel Marinho <isabelms2005@gmail.com>
2025-07-17 13:41:25 -0300

    Classe minigames atualizada

--- src/Minigames.java
+++ src/Minigames.java
@@ -6,15 +6,8 @@ public abstract class Minigames {
     public Minigames(String nome) {
         this.nome = nome;
         this.concluido = false;
-        this.recompensa = 0;
+        this.recompensa = 0; // Pontuação ajustada em cada minijogo
     }
-
-
-    public abstract void iniciar();
-
-    public abstract boolean verificarVitoria();
-
-    public abstract void exibirRegras();
 
     public String getNome() {
         return nome;
@@ -24,21 +17,21 @@ public abstract class Minigames {
         return concluido;
     }
 
+    public void setConcluido(boolean concluido) {
+        this.concluido = concluido;
+    }
+
     public int getRecompensa() {
         return recompensa;
     }
 
-    // Métodos set para atualizar o estado e recompensa, se necessário
-    protected void setConcluido(boolean concluido) {
-        this.concluido = concluido;
-        if(getNome().equals("Codificador")||getNome().equals("Decodificador")){
-            this.recompensa = 1;
-        } else if (getNome().equals("Roleta")) {
-            this.recompensa = //terminar codigo
-        }
-    }
-
-    protected void setRecompensa(int recompensa) {
+    public void setRecompensa(int recompensa) {
         this.recompensa = recompensa;
     }
+
+    public abstract void exibirRegras();
+
+    public abstract void iniciar();
+
+    public abstract boolean verificarVitoria();
 }
