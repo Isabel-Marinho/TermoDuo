e2a5f969025cae75250e413c460a58a7fa6b3a7a
Isabel Marinho <isabelms2005@gmail.com>
2025-07-17 13:04:37 -0300

    Início do Fim

--- src/Beatrix.java
+++ src/Beatrix.java
@@ -4,9 +4,9 @@ class Beatrix extends Personagem {
         super(nomeJogador, "Beatrix", "Inteligente, baixinha e irritada", 20, "Acerta dois números do minigame de codificação assim que começa o minijogo.", "Beatrix");
     }
 
-    public void modificaRegra(HashMap codigoSecreto, int tamanhoCodigo){
-        int penultimoDigito = (Integer) codigoSecreto.get(tamanhoCodigo - 2); // Penúltima posição (índice base 0)
-        int ultimoDigito = (Integer) codigoSecreto.get(tamanhoCodigo - 1); // Última posição (índice base 0)
+    public void modificaRegra(HashMap<Integer, Integer> codigoSecreto, int tamanhoCodigo){
+        int penultimoDigito = codigoSecreto.get(tamanhoCodigo - 2); // Penúltima posição
+        int ultimoDigito = codigoSecreto.get(tamanhoCodigo - 1); // Última posição
 
         System.out.println("  > O dígito na posição " + (tamanhoCodigo - 1) + " (penúltima) é: " + penultimoDigito);
         System.out.println("  > O dígito na posição " + (tamanhoCodigo) + " (última) é: " + ultimoDigito);
